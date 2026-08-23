448387cba95b10d240c75f2539c066e1df8ec414
NeonGD1667 <neongmd1667@gmail.com>
2026-08-23 09:45:18 +0700

    Add token management to CandyVersusSession
    Added a token member variable and updated related methods.

--- src/CandyVersusSession.hpp
+++ src/CandyVersusSession.hpp
@@ -9,6 +9,7 @@ protected:
 
     bool m_loggedIn = false;
     std::string m_username;
+    std::string m_token;
     int m_elo = 0;
 
 public:
@@ -19,18 +20,23 @@ public:
 
     bool isLoggedIn() const { return m_loggedIn; }
     std::string getUsername() const { return m_username; }
+    std::string getToken() const { return m_token; }
     int getElo() const { return m_elo; }
 
-    void setLoggedIn(std::string const& username, int elo) {
+    void setElo(int elo) { m_elo = elo; }
+
+    void setLoggedIn(std::string const& username, int elo, std::string const& token) {
         m_loggedIn = true;
         m_username = username;
         m_elo = elo;
+        m_token = token;
         log::info("Session: logged in as {} (Elo: {})", username, elo);
     }
 
     void logout() {
         m_loggedIn = false;
         m_username.clear();
+        m_token.clear();
         m_elo = 0;
     }
 };

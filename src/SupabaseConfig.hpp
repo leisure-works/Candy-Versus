2185ded8fe16e88ac485b37412c861b5ef9c5945
NeonGD1667 <neongmd1667@gmail.com>
2026-08-23 09:41:14 +0700

    Create SupabaseConfig.hpp

---
+++ src/SupabaseConfig.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace SupabaseConfig {
+    inline constexpr auto URL = "https://vwsmthfwhwzzlqtzsjvp.supabase.co";
+    inline constexpr auto ANON_KEY =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3c210aGZ3aHd6emxxdHpzanZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMzcwNzIsImV4cCI6MjEwMjcxMzA3Mn0.Whzscv-OuixVRBpplx1_t_73hnLdv9ktEylUxEsOC3c";
+}
